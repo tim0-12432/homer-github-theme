d2de632fa1b055a6b4d88c4fc3c4bfe58c468b02
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2023-04-27 17:26:56 +0200

    add different color schemes

--- utils/generate-minified-css.js
+++ utils/generate-minified-css.js
@@ -2,15 +2,22 @@ const { minify } = require('csso');
 const path = require('path');
 const fs = require('fs');
 
-const cssPath = path.resolve(__dirname, '../assets/gh-theme.css');
-console.log("### Got the path to CSS:", cssPath);
+function generate(res) {
+    const cssPath = path.resolve(__dirname, `../assets/gh-${res}.css`);
+    console.log("### Got the path to CSS:", cssPath);
 
-const css = fs.readFileSync(cssPath, 'utf8');
-console.log("### Read the CSS:", css.replace(/(\r\n|\n|\r)/gm, " ").substring(0, 50) + "...");
+    const css = fs.readFileSync(cssPath, 'utf8');
+    console.log("### Read the CSS:", css.replace(/(\r\n|\n|\r)/gm, " ").substring(0, 50) + "...");
 
-const minifiedCss = minify(css).css;
-console.log("### Minified the CSS:", minifiedCss.substring(0, 50) + "...");
+    const minifiedCss = minify(css).css;
+    console.log("### Minified the CSS:", minifiedCss.substring(0, 50) + "...");
 
-const minifiedCssPath = path.resolve(__dirname, '../assets/gh-theme.min.css');
-fs.writeFileSync(minifiedCssPath, minifiedCss);
-console.log("### Wrote the minified CSS to:", minifiedCssPath);
+    const minifiedCssPath = path.resolve(__dirname, `../assets/gh-${res}.min.css`);
+    fs.writeFileSync(minifiedCssPath, minifiedCss);
+    console.log("### Wrote the minified CSS to:", minifiedCssPath);
+}
+
+generate("base");
+generate("dimmed");
+generate("light");
+generate("dark");
--- utils/lint-css.js
+++ utils/lint-css.js
@@ -2,29 +2,36 @@ const stylelint = require('stylelint');
 const path = require('path');
 const fs = require('fs');
 
-const cssPath = path.resolve(__dirname, '../assets/gh-theme.css');
-console.log("### Got the path to CSS:", cssPath);
+function lint(res) {
+    const cssPath = path.resolve(__dirname, `../assets/gh-${res}.css`);
+    console.log("### Got the path to CSS:", cssPath);
 
-const css = fs.readFileSync(cssPath, 'utf8');
-console.log("### Read the CSS:", css.replace(/(\r\n|\n|\r)/gm, " ").substring(0, 50) + "...");
+    const css = fs.readFileSync(cssPath, 'utf8');
+    console.log("### Read the CSS:", css.replace(/(\r\n|\n|\r)/gm, " ").substring(0, 50) + "...");
 
-stylelint.lint({
-    code: css,
-    config: {
-        extends: 'stylelint-config-standard',
-        rules: {},
-    },
-}).then((data) => {
-    const warnings = data.results[0].warnings;
-    console.log("### Linted the CSS:", warnings.length, "warnings found.");
-    const report = warnings.map((warning) => {
-        const string = warning.severity +
-            " (" + warning.line + ":" + warning.column + "): " +
-            warning.text;
-        console.log("# -- " + string);
-        return string;
-    }).join("\n");
-    fs.writeFileSync(path.resolve(__dirname, '../linting-report.txt'), report);
-}).catch((err) => {
-    console.error("### Error linting the CSS:", err);
-});
+    stylelint.lint({
+        code: css,
+        config: {
+            extends: 'stylelint-config-standard',
+            rules: {},
+        },
+    }).then((data) => {
+        const warnings = data.results[0].warnings;
+        console.log("### Linted the CSS:", warnings.length, "warnings found.");
+        const report = warnings.map((warning) => {
+            const string = warning.severity +
+                " (" + warning.line + ":" + warning.column + "): " +
+                warning.text;
+            console.log("# -- " + string);
+            return string;
+        }).join("\n");
+        fs.writeFileSync(path.resolve(__dirname, '../linting-report.txt'), report);
+    }).catch((err) => {
+        console.error("### Error linting the CSS:", err);
+    });
+}
+
+lint("base");
+lint("dimmed");
+lint("light");
+lint("dark");
